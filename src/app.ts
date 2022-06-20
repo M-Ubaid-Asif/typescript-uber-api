40ad709c1b1434473f56584351e805bec8d61b13
mohammad ubaid <ubaid6664@gmail.com>
2022-06-20 16:44:28 +0530

    boiler plate setup

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import logger from './config/logger'
+import express, { Request, Response, NextFunction, Application } from 'express'
+import errorHandler from './helpers/errorHandler'
+
+const app: Application = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// loggin all the requests in middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info(
+    `METHOD - [${req.method}], URL-[${req.url}], IP - [${req.socket.remoteAddress}]`
+  )
+
+  res.on('finish', () => {
+    logger.info(
+      `METHOD - [${req.method}], URL-[${req.url}], IP - [${req.socket.remoteAddress}], status - [${res.statusCode}]`
+    )
+  })
+  next()
+})
+
+// routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world')
+})
+
+// error handler
+app.use(errorHandler)
+
+// 404
+app.use((req: Request, res: Response) => {
+  const error = new Error('not found')
+
+  return res.status(404).json({
+    message: error.message,
+  })
+})
+
+export default app
