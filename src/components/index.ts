794c7fe8e012eec88635bd529154209c123dac24
mohammad ubaid <ubaid6664@gmail.com>
2022-06-23 15:00:46 +0530

    added booking route

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import userRouter from './user/userRoutes'
 import cabRouter from './cab/cabRoutes'
-
+import bookRouter from './booking/bookingRoutes'
 const router = express.Router()
 
 router.use('/user', userRouter)
 router.use('/cab', cabRouter)
+router.use('/booking', bookRouter)
 export default router
